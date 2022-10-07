a8369483ce87b4c8d5be0ed6ce915ef15c63385b
sriwahyunilkhotimah <114728068+sriwahyunilkhotimah@users.noreply.github.com>
2022-10-07 16:16:19 +0700

    Add files via upload

---
+++ session3.py
@@ -0,0 +1,23 @@
+from email.quoprimime import quote
+from unicodedata import name
+
+
+Name ="Sri wahyuni" 
+Kelas = "TI22I"
+Major= "Informatic Engineering"
+Age= 19
+Quote= "Just Focus on Your Next Step"
+ilikespicy= True
+hobbies=["swim","hiking","traveling",]
+
+#display 
+print("hallo! my name is : ", Name)
+print("i'm a student of : ", Kelas )
+print("im majoring : ", Major)
+print("my age is : ", Age)
+print("quotes life : ", Quote)
+print("i like spicy : ", ilikespicy)
+print("my hobby : ", hobbies)
+
+
+
