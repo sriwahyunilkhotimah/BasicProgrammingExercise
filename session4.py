90492b23b8e3c3db9d0c72243835caaad50d72b0
sriwahyunilkhotimah <114728068+sriwahyunilkhotimah@users.noreply.github.com>
2022-10-14 16:36:56 +0700

    session4

---
+++ session4.py
@@ -0,0 +1,29 @@
+from operator import itemgetter
+
+
+
+#input product name to variablebnj
+product_name = input(" input the product name : ")
+#input product price to variable
+product_price = int(input(" input price of product: "))
+#set 10% variable
+persen= 10
+#input amount of items
+amount_items = int(input(" how many items do you buy : " ))
+#harga product * jumlah yang di beli
+print ( " money spent " ,product_price*amount_items)
+#calculate the 10% from the produk price
+profit= product_price*persen/100
+profit_items= profit* amount_items
+print ("profit 10% : " + str(profit))
+print("total profit : " + str(profit_items))
+#print the final product price
+print(product_name,"sale price is : " , product_price + profit)
+#price sale
+price_sale = product_price+profit
+total_selling = price_sale*amount_items
+print(" total sale price" ,total_selling)
+#item sold
+item_sold = int (input("how many product have been sold : "))
+profit_sold = profit * item_sold
+print("profit from items sold : " + str(profit_sold))
